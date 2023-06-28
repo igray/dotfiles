ed6eb064dc1f7e2d347fd7994fa4ad9a9542f5a2
Iain Gray <igray78756@gmail.com>
2023-06-27 19:50:51 -0500

    Added ciboost

--- fish/.config/fish/conf.d/careerplug.fish
+++ fish/.config/fish/conf.d/careerplug.fish
@@ -19,3 +19,21 @@ end
 function gpu
   git pull origin (currentGitBranch)
 end
+
+function gpf
+  git push --force-with-lease
+end
+
+function ciboost
+  set branch (currentGitBranch)
+  if test "$branch" = "main"
+    echo "You are on main branch, please checkout to a feature branch"
+    return 1
+  end
+  gco main
+  gpu
+  gco "$branch"
+  git rebase main
+  git show -s --format=%B | sed '1 s/\( \[ciboost\]\)\?$/ [ciboost]/' | git commit --amend -F -
+  gpf
+end
