fc60c6966be3496ef1b38722c88b4fdea5861f91
Iain Gray <igray78756@gmail.com>
2024-06-04 18:45:53 -0500

    Added hardware for laptop

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";                     # Stable Nix Packages (Default)
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";         # Unstable Nix Packages
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,7 +25,7 @@
     };
   };
 
-  outputs = { home-manager, nixpkgs, nixpkgs-unstable, ... }@inputs:
+  outputs = { home-manager, nixpkgs, nixpkgs-unstable, nixos-hardware, ... }@inputs:
   let
     vars = {
       username = "igray";
@@ -44,7 +45,10 @@
   {
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs vars system; };
-      modules = [ ./nixos/configuration.nix ];
+      modules = [
+         nixos-hardware.nixosModules.framework-13-7040-amd
+        ./nixos/configuration.nix
+      ];
     };
 
     homeConfigurations."${vars.username}" = home-manager.lib.homeManagerConfiguration {
