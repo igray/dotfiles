2ccc6a1afbe8969874e01ae4c6587ad403fc6037
Iain Gray <igray78756@gmail.com>
2024-11-10 15:21:29 -0600

    Adjusted restic script to work from home or remote

--- bin/restic-backups-work.sh
+++ bin/restic-backups-work.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+
+HOME_SERVER=192.168.86.10
+#HOME_SERVER=home.igster.org
+
 export RESTIC_CACHE_DIR=/var/cache/restic-backups-work
 export RESTIC_PASSWORD_FILE=/home/igray/.restic-password
-export RESTIC_REPOSITORY=sftp:igray@home.igster.org:/media/backup/restic
+export RESTIC_REPOSITORY="sftp:igray@$HOME_SERVER:/media/backup/restic"
 
-restic -o sftp.command='ssh igray@home.igster.org -i /home/igray/.ssh/restic -s sftp' "$@"
+restic -o sftp.command="ssh igray@$HOME_SERVER -i /home/igray/.ssh/restic -s sftp" "$@"
 
 unset RESTIC_CACHE_DIR
 unset RESTIC_PASSWORD_FILE
