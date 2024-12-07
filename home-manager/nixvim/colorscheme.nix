82623f50bea6e92383c5080087f49f6e0716073d
Iain Gray <igray78756@gmail.com>
2024-12-07 14:29:28 -0600

    Stop managing plugins

--- home-manager/nixvim/colorscheme.nix
+++ home-manager/nixvim/colorscheme.nix
@@ -1,16 +1,8 @@
 { pkgs, ... }:
 {
-  extraPlugins = [(pkgs.vimUtils.buildVimPlugin {
-    name = "solarized";
-    isColorscheme = true;
-    originalName = "nvim-solarized-lua";
-    src = pkgs.fetchFromGitHub {
-      owner = "ishan9299";
-      repo = "nvim-solarized-lua";
-      rev = "d69a263c97cbc765ca442d682b3283aefd61d4ac";
-      hash = "sha256-0NABkr2d86Uq3OU4lbn2dyjRbwE3+5euqh1CA+MwDtQ=";
-    };
-  })];
+  extraPlugins = with pkgs.vimPlugins; [
+    nvim-solarized-lua
+  ];
   colorscheme = "solarized";
   globals = {
     solarized_visibility = "low";
