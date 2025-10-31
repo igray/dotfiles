9afdae96fd78e4e6f7bbf08f881d6087879c302e
Iain Gray <igray78756@gmail.com>
2025-10-31 16:12:49 -0500

    Adjust for guardian's new html.

--- home-manager/wallpaper.nix
+++ home-manager/wallpaper.nix
@@ -15,18 +15,26 @@
 
         rss = Nokogiri::XML(URI.open('https://www.theguardian.com/news/series/ten-best-photographs-of-the-day/rss'))
         pod_url = rss.xpath('//item/link').first.content
-        exit if File.exist?(CFG) && pod_url == open(CFG).read.strip
+        puts "pod_url: #{pod_url}"
+        last_url = open(CFG).read.strip
+        puts "last_url: #{last_url}"
+        exit if File.exist?(CFG) && pod_url == last_url
 
         doc = URI.open(pod_url).read
         img_data = {}
-        doc.scan(/srcset="([^"]+)"/)
+        doc.scan(/srcSet="([^"]+)"/)
           .flatten
           .reject { |val| val.include?(',') }
-          .each do |srcpair|
-          fullpath, rawwidth = srcpair.split(' ')
-          filename = URI.parse(fullpath).path.split('/').last
-          width = rawwidth.to_i
-          next if img_data.key?(filename) && img_data[filename][:width] > width
+          .each do |fullpath|
+
+          uri = URI.parse(fullpath)
+          filename = uri.path.split('/').last
+          params = Hash[URI.decode_www_form(uri.query.gsub('&amp;','&'))]
+          width = params['width'].to_i
+          if img_data.key?(filename) && img_data[filename][:width] > width
+            puts "Skipping #{filename} with width #{width}"
+            next
+          end
 
           img_data[filename] = {
             fullpath: fullpath.gsub('&amp;', '&'),
@@ -34,6 +42,7 @@
             width: width
           }
         end
+        puts "Found #{img_data.length} images"
         if img_data.length > 0
           `rm -f #{DIR}/*`
           img_data.values.each do |d|
