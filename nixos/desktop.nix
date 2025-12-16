c6d31b0a65344d7caaa6fef3824dd138178c1b3b
Iain Gray <igray78756@gmail.com>
2025-12-15 21:41:37 -0600

    Removed extra polkit service

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -44,20 +44,4 @@
       _JAVA_AWT_WM_NONREPARENTING = "1";
     };
   };
-
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-  };
 }
