776bd8aedd60f9fbe0a1c7bb2c3a27536e596695
Iain Gray <igray78756@gmail.com>
2026-06-07 15:07:43 -0500

    Update

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     claude-code.url = "github:sadjow/claude-code-nix";
-    claude-desktop.url = "github:aaddrick/claude-desktop-debian?ref=v2.0.16%2Bclaude1.9255.2";
+    claude-desktop.url = "github:aaddrick/claude-desktop-debian?ref=v2.0.18%2Bclaude1.11187.4";
   };
 
   outputs =
