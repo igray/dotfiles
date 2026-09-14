1372e1c0c1b5eb95f9fdc2c68997c8fc76ff1623
Iain Gray <igray78756@gmail.com>
2026-09-14 10:57:05 -0500

    Notify cosmic after downloading new wallpapers

--- home-manager/wallpaper.nix
+++ home-manager/wallpaper.nix
@@ -7,8 +7,10 @@
         #! /usr/bin/env nix-shell
         #! nix-shell -i ruby -p "ruby.withPackages (ps: with ps; [ nokogiri ])"
 
+        require 'fileutils';
         require 'nokogiri';
         require 'open-uri';
+        require 'tmpdir';
 
         CFG="#{ENV['HOME']}/.local/wallchange/guardian_pod"
         DIR="#{ENV['HOME']}/Pictures/wallpapers"
@@ -16,9 +18,9 @@
         rss = Nokogiri::XML(URI.open('https://www.theguardian.com/news/series/ten-best-photographs-of-the-day/rss'))
         pod_url = rss.xpath('//item/link').first.content
         puts "pod_url: #{pod_url}"
-        last_url = open(CFG).read.strip
+        last_url = File.exist?(CFG) ? File.read(CFG).strip : nil
         puts "last_url: #{last_url}"
-        exit if File.exist?(CFG) && pod_url == last_url
+        exit if pod_url == last_url
 
         doc = URI.open(pod_url).read
         img_data = {}
@@ -44,11 +46,19 @@
         end
         puts "Found #{img_data.length} images"
         if img_data.length > 0
-          `rm -f #{DIR}/*`
+          tmp = Dir.mktmpdir('wallchange', File.dirname(DIR))
           img_data.values.each do |d|
-            IO.copy_stream(URI.open(d[:fullpath]), d[:local])
+            IO.copy_stream(URI.open(d[:fullpath]), File.join(tmp, File.basename(d[:local])))
           end
-          open(CFG, 'w') { |io| io.puts pod_url }
+          Dir.glob("#{DIR}/*").each { |f| File.delete(f) }
+          Dir.glob("#{tmp}/*").each { |f| File.rename(f, File.join(DIR, File.basename(f))) }
+          Dir.rmdir(tmp)
+          FileUtils.mkdir_p(File.dirname(CFG))
+          File.write(CFG, "#{pod_url}\n")
+
+          # cosmic-bg 1.6 never re-scans a slideshow dir; rewriting this key makes it reload.
+          same = "#{ENV['HOME']}/.config/cosmic/com.system76.CosmicBackground/v1/same-on-all"
+          File.write(same, File.read(same)) if File.exist?(same)
         end
       '';
       executable = true;
